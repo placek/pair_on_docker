b42835dea3b326755564d8073772a7a3ee039220
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-12-11 12:26:07 +0100

    manipulate with .authorized_keys file

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
-FROM ruby:2.4.1-alpine3.6
-MAINTAINER Paweł Placzyński <placzynski.pawel@gmail.com>
+FROM alpine:latest
+
+RUN apk add --update --no-cache openssh tmux vim && \
+    ssh-keygen -A && \
+    adduser -D -s /bin/ash pair && \
+    echo "pair:$(date +%s)" | chpasswd && \
+    mkdir /home/pair/.ssh && touch /home/pair/.ssh/authorized_keys && \
+    chown -R pair:pair /home/pair
 
 EXPOSE 22
-ENTRYPOINT ["/bin/entrypoint"]
+ENTRYPOINT /bin/entrypoint
 
 ADD fs/sshd_config /etc/ssh/sshd_config
 ADD fs/entrypoint.sh /bin/entrypoint
-
-RUN \
-  apk add --update --no-cache openssh tmux curl && \
-  ssh-keygen -A && \
-  adduser -D -s /bin/ash pair && \
-  echo "pair:$(date +%s)" | chpasswd && \
-  mkdir /home/pair/.ssh && touch /home/pair/.ssh/authorized_keys && \
-  chown -R pair:pair /home/pair
--- fs/entrypoint.sh
+++ fs/entrypoint.sh
@@ -1,31 +1,18 @@
 #!/bin/ash
 
-# when no USERS variable set exit with errcode 1
-if [ -z "$@" ]
-then
-   >&2 echo "ERROR: You need to provide LDAP logins!"
-   exit 1
+# exit when no keys available under .authorized_keys
+if [ ! -f "/home/pair/workspace/.authorized_keys" ]; then
+  >&2 echo "ERROR: no SSH keys in .authorized_keys"
+  exit 1
 fi
 
-for user in $@
-do
-  # fetch pubkeys by LDAP user login
-  echo "Fetching key for $user..."
-  KEY=$(su -c "curl https://keys.binarapps.com/$user 2>/dev/null")
-
-  # on fetch fail exit with errcode 2
-  if [ "$KEY" = "not found" ]
-  then
-   >&2 echo "ERROR: Failed fetching key for $user!"
-   exit 2
-  fi
-
-  # append key to authorized_keys
-  su -c "echo 'command=\"/usr/bin/tmux new -s $user -t pair\" $KEY' >> /home/pair/.ssh/authorized_keys" pair
+# set proper .ssh/authorized_keys
+cat /home/pair/workspace/.authorized_keys | while read key; do
+  echo "command=\"/usr/bin/tmux attach -t pair\" $key" >> /home/pair/.ssh/authorized_keys
 done
 
 # start tmux named session "pair" in deteached mode
-su -c "tmux new -d -s pair -c /home/pair" pair
+su -c "tmux new -d -s pair -c /home/pair/workspace" pair
 
 # start ssh daemon
 exec /usr/sbin/sshd -Def /etc/ssh/sshd_config
